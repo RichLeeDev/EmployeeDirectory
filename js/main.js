619d28359c7a056cd4272cd7e6dccac113fdecbd
Richard L <48262199+RichLeeDev@users.noreply.github.com>
2020-09-03 22:53:25 -0700

    updated boxes

--- js/main.js
+++ js/main.js
@@ -23,18 +23,20 @@ let name = employee.name;
 let email = employee.email;
 let city = employee.location.city;
 let picture = employee.picture;
+
 // template literals make this so much cleaner!
 employeeHTML += `
-<div class="card" data-index="${index}">
-<img class="avatar" src="${picture.large}" />
-<div class="text-container">
-<h2 class="name">${name.first} ${name.last}</h2>
-<p class="email">${email}</p>
-<p class="address">${city}</p>
-</div>
-</div>
+  <div class="card" data-index="${index}">
+    <img class="avatar" src="${picture.large}" />
+    <div class="text-container">
+      <h2 class="name">${name.first} ${name.last}</h2>
+      <p class="email">${email}</p>
+      <p class="address">${city}</p>
+    </div>
+  </div>
 `
 });
+
 gridContainer.innerHTML = employeeHTML;
 }
 
@@ -42,7 +44,9 @@ function displayModal(index) {
 // use object destructuring make our template literal cleaner
 let { name, dob, phone, email, location: { city, street, state, postcode
 }, picture } = employees[index];
+
 let date = new Date(dob.date);
+
 const modalHTML = `
 <img class="avatar" src="${picture.large}" />
 <div class="text-container">
@@ -64,7 +68,6 @@ gridContainer.addEventListener('click', e => {
 // make sure the click is not on the gridContainer itself
 if (e.target !== gridContainer) {
 // select the card element based on its proximity to actual element
-clicked
 const card = e.target.closest(".card");
 const index = card.getAttribute('data-index');
 displayModal(index);
